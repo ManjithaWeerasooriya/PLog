0f8f1b45f1c63b808dd0261b3c5fed43e9065633
Manjitha Weerasooriya <manjithawaa@gmail.com>
2026-09-16 01:18:19 +0530

    Initial Commit

---
+++ PLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PLog
+//
+//  Created by Manjitha Weerasooriya on 2026-09-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PLog/PLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  PLogApp.swift
+//  PLog
+//
+//  Created by Manjitha Weerasooriya on 2026-09-16.
+//
+
+import SwiftUI
+
+@main
+struct PLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
